d44fa7962c0ce34c556403aac95701852675365f
Akerman048 <akkerman048@gmail.com>
2025-03-19 16:48:39 +0000

    responsive slider

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -72,7 +72,7 @@ export const Home = () => {
         scrollToPreviousSection(); // Скрол вгору
       }
     },
-    [isScrolling, scrollToNextSection, scrollToPreviousSection] // ✅ Додаємо стабільні залежності
+    [isScrolling , scrollToNextSection, scrollToPreviousSection] // ✅ Додаємо стабільні залежності
   );
 
   // Відстеження поточного компонента через IntersectionObserver
