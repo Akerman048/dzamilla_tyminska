bebb35b77296aac0bf7985c608e914ffacdeb78a
Akerman048 <akkerman048@gmail.com>
2025-04-19 12:36:30 +0000

    responsive slider

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { Outlet } from "react-router-dom";
 import s from "./Layout.module.css"
 
 export const Layout = () => {
+
+  useEffect(() => {
+    const handleContextMenu = (e) => {
+      e.preventDefault();
+    };
+
+    document.addEventListener("contextmenu", handleContextMenu);
+
+    return () => {
+      document.removeEventListener("contextmenu", handleContextMenu);
+    };
+  }, []);
+
   return (
     <>
       <div className={s.container}>
